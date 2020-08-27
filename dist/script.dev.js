311a17181ef1d69196a3187af54efd0a7facacbe
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-08-27 09:26:50 +0200

    first commit

---
+++ dist/script.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function ingredientsHover() {
+  document.getElementById("ingredients").firstElementChild.firstElementChild.style.fontSize = "200%";
+}
+
+function ingredientsNormal() {
+  document.getElementById("ingredients").firstElementChild.firstElementChild.style.fontSize = "100%";
+}
+
+function preparationHover() {
+  document.getElementById("preparation").firstElementChild.firstElementChild.style.fontSize = "200%";
+}
+
+function preparationNormal() {
+  document.getElementById("preparation").firstElementChild.firstElementChild.style.fontSize = "100%";
+}
